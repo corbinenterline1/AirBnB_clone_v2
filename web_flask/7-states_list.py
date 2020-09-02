45a2965cd9c663f9e879b0fa4c8f8adb660169be
Corbin Enterline <corbinenterline1@gmail.com>
2020-09-02 20:59:44 +0000

    lost track of numbers

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,6 +7,49 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route('/', strict_slashes=False)
+def heybuddy():
+    """Returns 'Hello HBNB!' """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns 'HBNB' """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def textual(text):
+    """Displays "C <text>, no underscores, undaspaces"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pytext(text="is cool"):
+    """Displays "Python <text>, no underscores, undaspaces"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def numbas(n):
+    """Displays "<n> is a number" only if n is an integer"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numbas_temp(n):
+    """Displays a HTML page with "Number: <n>", H1 tag, inside BODY"""
+    return render_template("5-number.html", n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def numbas_oddeven_temp(n):
+    """Displays a HTML page with "Number: <n> is even|odd", H1, in BODY"""
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
 @app.route('/states_list', strict_slashes=False)
 def listates():
     """Displays an HTML page with a list of all State objects, sorted"""
@@ -18,6 +61,7 @@ def listates():
 def ripnshred(self):
     """Removes current SQLAlchemy Session"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
